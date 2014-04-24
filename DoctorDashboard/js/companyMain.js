91bebd79466c623f84f36e798a0017971625736d
Jordan L <jsl20038@gmail.com>
2014-04-24 13:19:24 -0400

    Added default date functionality.

--- DoctorDashboard/js/companyMain.js
+++ DoctorDashboard/js/companyMain.js
@@ -1007,6 +1007,16 @@ $(document).ready(function () {
 	load();
 	getDoctorProfile();
 	loadUnreadMessages();
+	var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+    if (month < 10) month = "0" + month;
+    if (day < 10) day = "0" + day;
+    var today = year + "-" + month + "-" + day;
+    $("#apptday").attr("value", today);   
+    displayAppointments();    
+    $("#visitdate").attr("value", today);
 	$('#recordVisit').on('hidden', function() {
 		$('#vSystolicBP').val("");
 	});
